49f590575c0576f60c055fd9e52c4f7e7cd291a7
Swati Vilas Tole <swatitole@Swatis-MacBook.local>
2020-07-07 19:31:37 +0800

    Initial Commit

---
+++ Project 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 2
+//
+//  Created by Swati Vilas Tole on 7/7/20.
+//  Copyright © 2020 Swati Vilas Tole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
